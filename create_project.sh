eb82c49223b482d6cd51844b8c915e173f41a1ab
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-04 10:59:00 -0600

    adds tasks.json to template adds section5

--- create_project.sh
+++ create_project.sh
@@ -6,8 +6,12 @@ if [ -z "$project_name" ]
 then
 	echo "\$project_name is empty"
 else
-	
-	mkdir -p $project_name/src
+	mkdir $project_name
 	cp ./template/compile.sh ./$project_name/compile.sh
+	
+	mkdir $project_name/src
 	cp ./template/src/main.cpp ./$project_name/src/main.cpp	
+
+	mkdir $project_name/.vscode
+	cp ./template/.vscode/tasks.json ./$project_name/.vscode/tasks.json
 fi
