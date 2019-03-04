b591622a2717949ea7d9f3670426091f8290951e
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-04 14:43:57 -0600

    WIP: asks user for number of large and small rooms

--- section6-challenge/src/main.cpp
+++ section6-challenge/src/main.cpp
@@ -9,16 +9,23 @@ int main() {
 	cout << "Frank's Carpet Cleaning Service" << endl;
 	
 	cout << "\nCharges and stipulations: " << endl;
-	const double smallRoomCharge = 25.0;
-	const double largeRoomCharge = 35.0;
-	const double tax_rate = 0.06;
-	const int estimate_experiation = 30; //days
+	const double smallRoomCharge {25.0};
+	const double largeRoomCharge {35.0};
+	const double tax_rate {0.06};
+	const int estimate_experiation {30}; //days
 	cout << "	$" << smallRoomCharge << " per small room." << endl;
 	cout << "	$" << largeRoomCharge << " per large room." << endl;
 	cout << "	Sales tax rate: " << tax_rate * 100 << "\%." << endl;
 	cout << "	Estimates are valid for " << estimate_experiation << " days." << endl;
 
 	cout << "\nHow many small rooms? " << endl;
+	int numberSmallRooms {0};
+	cin >> numberSmallRooms;
+	cout << "How many large rooms? " << endl;
+	int numberLargeRooms {0};
+	cin >> numberLargeRooms;
+
+	cout << "\nEstimate for carpet cleaning service";
 
 	cout << "\n*******************************" << endl;
 
