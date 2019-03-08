78236c46e190f74c43390d84863a7f42509b0795
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-08 11:31:02 -0600

    adds completed section 10 encryption challenge

---
+++ section10-challenge/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    
+    string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    string key      {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+
+	cout << "Enter your secret message?" << endl;
+	string inputMessage {};
+	std::getline(cin, inputMessage);
+
+	cout << "\nEncrypting message..." << endl;
+	string encryptedMessage {};
+
+	for (char c : inputMessage) {
+		size_t position = alphabet.find(c);
+		char lookupChar = key.at(position);
+		encryptedMessage += lookupChar;
+	}
+
+	cout << "\nEncrypted message: " << endl;
+	cout << encryptedMessage << endl;
+    
+    cout << endl;
+    return 0;
+}
