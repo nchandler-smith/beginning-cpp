4fa5acd40a96580b7e85d27eb54a673ecb5393c2
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-09 14:52:03 -0600

    adds processEntry and rough performAction

--- section11-challenge/src/main.cpp
+++ section11-challenge/src/main.cpp
@@ -2,14 +2,18 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 
 void displayMenu();
+char processEntry();
+bool performAction(char action);
 
 int main() {
+	char action {};
 
 	displayMenu();
-	// validate entry
-	// perform action
+	action = processEntry();
+	performAction(action);
 	// loop or exit
 
 	return 0;
@@ -23,4 +27,24 @@ void displayMenu() {
 	cout << "S - Display smallest number in list" << endl;
 	cout << "L - Display largest number in list" << endl;
 	cout << "Q - Quit \n" << endl;
+}
+
+char processEntry() {
+	char userResponse {};
+	cin >> userResponse;
+	return std::toupper(userResponse);
+}
+
+bool performAction(char action){
+	
+	switch(action) {
+		case 'P': cout << "P pressed" << endl; break;
+		case 'A': cout << "A pressed" << endl; break;
+		case 'M': cout << "M pressed" << endl; break;
+		case 'S': cout << "S pressed" << endl; break;
+		case 'L': cout << "L pressed" << endl; break;
+		case 'Q': cout << "Q pressed" << endl; break;
+	}
+
+	return false;
 }
