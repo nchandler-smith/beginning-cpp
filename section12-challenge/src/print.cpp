386cf86c5a8e3b2f0237e38869fba96f685aaa36
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-12 10:38:48 -0500

    print calls pretty_print

--- section12-challenge/src/print.cpp
+++ section12-challenge/src/print.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <string>
+#include "print.h"
 
 using namespace std;
 
 void print(const int array_in[], size_t array_size) {
-	cout << "[ ";
-	for(size_t i {}; i < array_size; i++) {
-		cout << (*array_in++) << " ";
-	}
-	cout << "]" << endl;
+	cout << pretty_print(array_in, array_size) << endl;
 }
 
 int *apply_all(int array1_in[], size_t array1_size, 
@@ -19,7 +16,7 @@ int *apply_all(int array1_in[], size_t array1_size,
 string pretty_print(const int array_in[], size_t array_size) {
     string return_string {"[ "};
     for(size_t i{}; i <array_size; i++) {
-        return_string += std::to_string(*(array_in++)) + " ";
+        return_string += std::to_string((array_in[i])) + " ";
     }
     return_string += "]";
   return return_string;
