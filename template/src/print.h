8e88a591e2c6451f8dd8b3f32d1a89fcafb81c72
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-13 10:21:20 -0500

    adds lean template that builds successfully

--- template/src/print.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-
-using std::string;
-
-string pretty_print(const int array_in[], size_t array_size);
-void print(const int array_in[], size_t array_size);
-int *apply_all(int array1_in[], size_t array1_size, int array2_in[], size_t array2_size);
