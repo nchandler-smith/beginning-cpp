f13b2449554ec97577c43bd96986beee7ec9ba3a
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-14 15:57:26 -0500

    initial commit of movies

---
+++ section13-challenge/test/test_movies.cpp
@@ -0,0 +1,9 @@
+#include "../googletest/googletest/include/gtest/gtest.h"
+#include "../googletest/googlemock/include/gmock/gmock.h"
+#include "../src/movies.h"
+
+TEST (MoviesTest, ChangeMyName) {
+    EXPECT_EQ(1, 1);
+}
+
+
