6f7fecdc0b4a5d4548d0b81eece583507236180a
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2020-02-16 15:36:01 -0600

    adding all the things

---
+++ .circleci/setup_directory.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# want a folder structure that looks like
+#   /root
+#       /courses
+#           /beginning-cpp
+#       /tools
+#           /googletest
+
+cd /root
+mkdir tools
+mkdir courses
+
+cd courses
+mkdir beginning-cpp
+cd beginning-cpp
+mv /root/project
