2e36b4c93c977fd0630ab58b3b8a85645dbdf366
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2020-03-02 12:37:58 -0600

    adds section 6 tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,9 @@
 
 cmake .
 make
+
+echo "Running random integer utility tests..."
 ./utils/random_integer/test/random_integer_test
+
+echo "Running section 6 tests..."
+./section6-challenge/test/test_estimation_tool
